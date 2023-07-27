3969a007b2dc557a3a443a6d7471ede946f4263d
Ahmed Kasu <ankassu@gmail.com>
2023-07-27 14:28:19 +0300

    added PatientListPage unit tests

--- patientor-frontend/src/PatientListPage/PatientListPage.test.tsx
+++ patientor-frontend/src/PatientListPage/PatientListPage.test.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
-import { BrowserRouter, MemoryRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
 
 import PatientListPage from '.'
 import { Gender } from '../types'
 
 describe('<PatientListPage />', () => {
+  const user = userEvent.setup()
   const patients = {
     'd2773336-f723-11e9-8f0b-362b9e111227': {
       id: 'd2773336-f723-11e9-8f0b-362b9e111227',
@@ -31,18 +33,22 @@ describe('<PatientListPage />', () => {
   })
 
   test('renders content', () => {
-    const header = screen.getByText('Patient list')
-    const addNewPatientButton = screen.getByText('Add New Patient')
-    const patientName1 = screen.getByText('Andre Onana')
-    const patientName2 = screen.getByText('Alycia Keys')
-    const patient1gender = screen.getByText('male')
-    const patient2gender = screen.getByText('female')
+    expect(screen.getByText('Patient list')).toBeDefined()
+    expect(screen.getByText('Add New Patient')).toBeDefined()
+    expect(screen.getByRole('table')).toBeDefined()
+    expect(screen.getByText('Andre Onana')).toBeDefined()
+    expect(screen.getByText('Alycia Keys')).toBeDefined()
+    expect(screen.getByText('male')).toBeDefined()
+    expect(screen.getByText('female')).toBeDefined()
+  })
 
-    expect(header).toBeDefined()
+  test('clicking add new patient button opens modal', async () => {
+    const addNewPatientButton = screen.getByText('Add New Patient')
     expect(addNewPatientButton).toBeDefined()
-    expect(patientName1).toBeDefined()
-    expect(patientName2).toBeDefined()
-    expect(patient1gender).toBeDefined()
-    expect(patient2gender).toBeDefined()
+
+    await user.click(addNewPatientButton)
+
+    expect(screen.getByRole('dialog')).toBeDefined()
+    expect(screen.getByText('Add a new patient')).toBeDefined()
   })
 })
