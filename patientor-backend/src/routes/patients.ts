220ae1e017abe2e39f7b3c0d51535b318e891818
Ahmed Kasu <ankassu@gmail.com>
2023-08-03 13:55:26 +0300

    modified patients route to fetch patients from mongodb

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,54 +1,57 @@
 import { Router } from 'express';
-import patientService from '../services/patientsService';
-import { NewPatient, PublicPatient, Entry } from '../types';
+// import { NewPatient, PublicPatient, Entry } from '../types';
 
-import toNewPatientInputs from '../utils/patientEntryHelpers';
-import toNewEntry from '../utils/newEntryHelper';
+// import toNewPatientInputs from '../utils/patientEntryHelpers';
+// import toNewEntry from '../utils/newEntryHelper';
+
+import { Patient } from '../mongo';
 
 const router = Router();
 
-router.get('/', (_req, res) => {
-  res.send(patientService.getPatients());
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+router.get('/', async (_req, res) => {
+  const patients = await Patient.find({});
+  res.send(patients);
 });
 
-router.get('/:id', (req, res) => {
-  const patient = patientService.getPatient(req.params.id);
-  if (patient) {
-    res.send(patient);
-  } else {
-    res.status(404).send('Patient not found');
-  }
-});
+// router.get('/:id', (req, res) => {
+//   const patient = patientService.getPatient(req.params.id);
+//   if (patient) {
+//     res.send(patient);
+//   } else {
+//     res.status(404).send('Patient not found');
+//   }
+// });
 
-router.post('/', (req, res) => {
-  try {
-    const newPatientEntry = toNewPatientInputs(req.body as NewPatient);
-    const newPatient: PublicPatient =
-      patientService.addPatient(newPatientEntry);
-    res.json(newPatient);
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    res.status(400).send(errorMessage);
-  }
-});
+// router.post('/', (req, res) => {
+//   try {
+//     const newPatientEntry = toNewPatientInputs(req.body as NewPatient);
+//     const newPatient: PublicPatient =
+//       patientService.addPatient(newPatientEntry);
+//     res.json(newPatient);
+//   } catch (error: unknown) {
+//     let errorMessage = 'Something went wrong.';
+//     if (error instanceof Error) {
+//       errorMessage += ' Error: ' + error.message;
+//     }
+//     res.status(400).send(errorMessage);
+//   }
+// });
 
-router.post('/:id/entries', (req, res) => {
-  try {
-    const newEntry = patientService.addEntry(
-      req.params.id,
-      toNewEntry(req.body as Entry)
-    );
-    res.json(newEntry);
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    res.status(400).send(errorMessage);
-  }
-});
+// router.post('/:id/entries', (req, res) => {
+//   try {
+//     const newEntry = patientService.addEntry(
+//       req.params.id,
+//       toNewEntry(req.body as Entry)
+//     );
+//     res.json(newEntry);
+//   } catch (error: unknown) {
+//     let errorMessage = 'Something went wrong.';
+//     if (error instanceof Error) {
+//       errorMessage += ' Error: ' + error.message;
+//     }
+//     res.status(400).send(errorMessage);
+//   }
+// });
 
 export default router;
