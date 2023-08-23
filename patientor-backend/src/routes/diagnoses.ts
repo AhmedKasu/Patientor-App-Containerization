386a9bc2b67beb44957015501ccbc743a1d2edc4
Ahmed Kasu <ankassu@gmail.com>
2023-08-23 22:00:51 +0300

    added async & error handler to diagnoses router

--- patientor-backend/src/routes/diagnoses.ts
+++ patientor-backend/src/routes/diagnoses.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import Diagnosis from '../mongo/models/Diagnosis';
+import { asyncHandler, errorHandler } from '../utils/middleware';
 
 const router = Router();
 
-router.get('/', (_req, res) => {
-  void (async () => {
-    const diagnosis = await Diagnosis.find({});
-    res.send(diagnosis);
-  })();
-});
+router.get(
+  '/',
+  asyncHandler(async (_req, res) => {
+    const diagnoses = await Diagnosis.find({});
+    res.send(diagnoses);
+  })
+);
 
+router.use(errorHandler);
 export default router;
