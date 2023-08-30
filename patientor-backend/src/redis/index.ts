daecef946bbe2b8dacac5684fa1923e2f6a92e78
Ahmed Kasu <ankassu@gmail.com>
2023-08-30 16:43:57 +0300

    added redis connection setup

---
+++ patientor-backend/src/redis/index.ts
@@ -0,0 +1,37 @@
+import { createClient } from 'redis';
+
+const client = createClient({
+  url: process.env.REDIS_URL,
+});
+
+void (async () => {
+  await client.connect();
+})();
+
+client.on('connect', () => console.log('connected to redis'));
+client.on('error', (err) => console.log('redis Client Error', err));
+
+const setCache = async (key: string, value: string) => {
+  try {
+    await client.set(key, value, {
+      EX: 1200,
+      NX: true,
+    });
+    return true;
+  } catch (err) {
+    console.error(`Error setting ${key} in Redis:`, err);
+    return false;
+  }
+};
+
+const getCache = async (key: string) => {
+  try {
+    const value = await client.get(key);
+    return value;
+  } catch (err) {
+    console.error(`Error getting ${key} from Redis:`, err);
+    return null;
+  }
+};
+
+export { setCache, getCache };
