b68a52ab780d550a90ebeb9b8d3ec28457b8b5a9
Ahmed Kasu <ankassu@gmail.com>
2023-09-05 21:58:42 +0300

    refactored valuesToSubmit helper function to use ladash omit

--- patientor-frontend/src/utils/addEntryFormHelper.ts
+++ patientor-frontend/src/utils/addEntryFormHelper.ts
@@ -1,28 +1,26 @@
 import { Entry, EntryFormValues, EntryType } from '../types';
+import _ from 'lodash';
 
-type UnionOmit<T, K extends string | number | symbol> = T extends unknown
-  ? Omit<T, K>
-  : never;
-type EntryWithoutId = UnionOmit<Entry, 'id'>;
+type EntryWithoutId = Omit<Entry, 'id'>;
 
 export const valuesToSubmit = (values: EntryFormValues): EntryWithoutId => {
-  let newValues: EntryWithoutId = { ...values, type: EntryType.HealthCheck };
+  let newValues: EntryWithoutId = { ...values };
 
   if (values.type === EntryType.HealthCheck) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { discharge, employerName, sickLeave, ...healthCheckValues } = values;
-    newValues = { ...healthCheckValues, type: EntryType.HealthCheck };
+    newValues = _.omit(values, ['discharge', 'employerName', 'sickLeave']);
   }
+
   if (values.type === EntryType.Hospital) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { healthCheckRating, employerName, sickLeave, ...hospitalValues } =
-      values;
-    newValues = { ...hospitalValues, type: EntryType.Hospital };
+    newValues = _.omit(values, [
+      'healthCheckRating',
+      'employerName',
+      'sickLeave',
+    ]);
   }
+
   if (values.type === EntryType.OccupationalHealthcare) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { healthCheckRating, discharge, ...healthCareValues } = values;
-    newValues = { ...healthCareValues, type: EntryType.OccupationalHealthcare };
+    newValues = _.omit(values, ['healthCheckRating', 'discharge']);
   }
+
   return newValues;
 };
