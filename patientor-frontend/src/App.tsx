379bfb3f04cbf584c2d119f2caa73b4da5abbb50
Ahmed Kasu <ankassu@gmail.com>
2023-09-11 15:01:15 +0300

    implemented refreshing user session once logged in, on page refresh & every 20 mins in 1hr

--- patientor-frontend/src/App.tsx
+++ patientor-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 
@@ -9,9 +9,35 @@ import RegisterUser from './AuthPages/Register';
 import Login from './AuthPages/Login';
 
 import usePatients from './hooks/usePatients';
+import { useAuthContext } from './context/authContext';
+import useRefreshUserSession from './hooks/useRefreshUserSession';
 
 const App = () => {
   const [patients] = usePatients();
+  const { currentUser } = useAuthContext();
+  const [intervalId, setIntervalId] = useState<number | null>(null);
+  const { refreshUserSession } = useRefreshUserSession();
+
+  useEffect(() => {
+    const fetchUserOnce = async () => {
+      const isUserAuthenticated = await refreshUserSession();
+      if (isUserAuthenticated && !intervalId) {
+        const id = window.setInterval(() => {
+          void refreshUserSession();
+        }, 20 * 60 * 1000);
+
+        setIntervalId(id);
+      }
+    };
+
+    void fetchUserOnce();
+
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [currentUser]);
 
   return (
     <div className='App'>
