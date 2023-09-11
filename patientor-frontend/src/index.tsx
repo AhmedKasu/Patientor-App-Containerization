c08c6895deda0676ddf810ec2bd0d1c7bd0b8279
Ahmed Kasu <ankassu@gmail.com>
2023-09-11 15:02:53 +0300

    added an api provider component/context (axios req headers),

--- patientor-frontend/src/index.tsx
+++ patientor-frontend/src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { PatientsProvider } from './context/patientsContext';
 import { AuthProvider } from './context/authContext';
+import ApiProvider from './context/ApiProvider';
 
 ReactDOM.render(
   <PatientsProvider>
     <AuthProvider>
-      <App />
+      <ApiProvider>
+        <App />
+      </ApiProvider>
     </AuthProvider>
   </PatientsProvider>,
 
