0c4b25d9aab5e047ae3b221713353441258d50b2
Ahmed Kasu <ankassu@gmail.com>
2023-09-12 12:41:16 +0300

    implemented logout success & error alerts

--- patientor-frontend/src/components/NavBar.tsx
+++ patientor-frontend/src/components/NavBar.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { Alert, AlertTitle } from '@material-ui/lab';
 
 import Box from '@mui/system/Box';
 import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
 
 import { useAuthContext } from '../context/authContext';
+import useLogout from '../hooks/useLogout';
 
 import UserProfileIcon from './UserProfileIcon';
 
 const NavBar = () => {
   const { currentUser } = useAuthContext();
-
+  const { logout, successAlert, errorAlert } = useLogout();
   const navigate = useNavigate();
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
         position: 'sticky',
         top: 0,
         zIndex: 1,
@@ -27,22 +27,48 @@ const NavBar = () => {
         p: 2,
         height: '4em',
       }}>
-      <Tooltip title='Home'>
-        <Typography
-          component={Link}
-          to='/'
-          variant='h3'
-          color='primary'
-          style={{ marginBottom: '0.5em', textDecoration: 'none' }}>
-          Patientor
-        </Typography>
-      </Tooltip>
-      <UserProfileIcon
-        currentUser={currentUser}
-        handleRegisterClick={() => navigate('/register')}
-        handleLoginClick={() => navigate('/login')}
-        handleLogoutClick={() => console.log('logout')}
-      />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          width: '100%',
+        }}>
+        <Tooltip title='Home'>
+          <Typography
+            component={Link}
+            to='/'
+            variant='h3'
+            color='primary'
+            style={{ marginBottom: '0.5em', textDecoration: 'none' }}>
+            Patientor
+          </Typography>
+        </Tooltip>
+
+        <UserProfileIcon
+          currentUser={currentUser}
+          handleRegisterClick={() => navigate('/register')}
+          handleLoginClick={() => navigate('/login')}
+          handleLogoutClick={() => logout()}
+        />
+      </Box>
+
+      {successAlert && (
+        <Alert
+          style={{ width: '50%', height: '100%', margin: 'auto' }}
+          severity='success'>
+          <AlertTitle>Success</AlertTitle>
+          Successfuly logged out.
+        </Alert>
+      )}
+
+      {errorAlert && (
+        <Alert
+          style={{ width: '50%', height: '100%', margin: 'auto' }}
+          severity='error'>
+          <AlertTitle>Error</AlertTitle>
+          Something went wrong. Please try again.
+        </Alert>
+      )}
     </Box>
   );
 };
