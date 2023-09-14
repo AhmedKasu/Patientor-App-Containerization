1f3163dd5be7ce12abf4c9c38c475e6b4f6e07be
Ahmed Kasu <ankassu@gmail.com>
2023-09-14 10:25:07 +0300

    fixed private routes layout to include navbar.

--- patientor-frontend/src/Routing/PrivateRoutes.tsx
+++ patientor-frontend/src/Routing/PrivateRoutes.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { Container } from '@material-ui/core';
+
 import { useAuthContext } from '../context/authContext';
+import NavBar from '../components/NavBar';
 
 const PrivateRoutes = () => {
   const { currentUser } = useAuthContext();
   if (!currentUser) return <Navigate to='/login' />;
 
-  return <Outlet />;
+  return (
+    <Container>
+      <NavBar />
+      <div id='App'>
+        <Outlet />
+      </div>
+    </Container>
+  );
 };
 
 export default PrivateRoutes;
